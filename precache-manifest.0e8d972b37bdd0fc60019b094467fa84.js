8f1e241a2f4d410d909668d57a3cb1045bb70df7
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2020-09-22 17:23:18 +0530

    Updates

---
+++ precache-manifest.0e8d972b37bdd0fc60019b094467fa84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f746759f14b8cf83baaf501a5fbc1d9d",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "bea49b31e7795a71ec7b",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea49b31e7795a71ec7b",
+    "url": "/react/static/js/main.52d84867.chunk.js"
+  },
+  {
+    "revision": "5436e33bbc49246e765c",
+    "url": "/react/static/js/runtime-main.2be02786.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
